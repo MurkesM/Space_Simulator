d8c2744184b525323720fab651744e5fb4ff87f0
MurkesM <ethanmartin377@yahoo.com>
2022-06-30 22:04:52 -0500

    started on an idle cam feature

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,12 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using UnityEngine.Playables;
 
 public class Player : MonoBehaviour
 {
-   [SerializeField] CinemachineVirtualCamera _cam3rdPerson;
-    
+    [SerializeField] CinemachineVirtualCamera _cam3rdPerson;
+    //[SerializeField] CinemachineVirtualCamera _camIdle;
+    //[SerializeField] PlayableDirector _directorIdle;
+    [SerializeField] GameObject _directorIdle;
+    [SerializeField] float _timeToIdle = 5;
+    [SerializeField] float _idle = -1f;
+
     void Update()
+    {
+        SwitchCams();
+
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    _directorIdle.SetActive(true);
+        //}
+        //else if (Input.GetKeyDown(KeyCode.Backspace))
+        //{
+        //    _directorIdle.SetActive(false);
+        //}
+
+        if (Time.time >= _timeToIdle && !Input.anyKey)
+        {
+            _directorIdle.SetActive(true);
+            _idle = Time.time + _timeToIdle;
+        }
+        else if (Time.time < _timeToIdle || Input.anyKey)
+        {
+            _directorIdle.SetActive(false);
+        }
+    }
+
+    void SwitchCams()
     {
         if (Input.GetKeyDown(KeyCode.R) && _cam3rdPerson.enabled == true)
         {
