92f58aaa27f3b6e32a24e3a3ca276c284e922048
MurkesM <ethanmartin377@yahoo.com>
2022-07-05 23:12:45 -0500

    Updated UI for contoler support and seperated intro and controls UI

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,8 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Playables;
 public class UIManager : MonoBehaviour
 {
+    //[SerializeField] PlayableDirector _controlsDirector;
+    //[SerializeField] PlayableDirector _introDirector;
+    [SerializeField] GameObject _introDirector;
+    [SerializeField] GameObject _controlDirector;
+    [SerializeField] Canvas _canvasIntro;
+    [SerializeField] Canvas _canvasControls;
+
     public void Restart()
     {
         SceneManager.LoadScene(1);
@@ -17,5 +25,33 @@ public class UIManager : MonoBehaviour
     public void StartGame()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void OnControlTransition()
+    {
+        //_controlsDirector.enabled = true;
+        //_introDirector.enabled = false;
+        _introDirector.SetActive(false);
+        _controlDirector.SetActive(true);
+        _canvasIntro.enabled = false;
+    }
+
+    public void ShowControls()
+    {
+        _canvasControls.enabled = true;
+    }
+
+    public void OnIntroTransition()
+    {
+        //_controlsDirector.enabled = false;
+        //_introDirector.enabled = true;
+        _introDirector.SetActive(true);
+        _controlDirector.SetActive(false);
+        _canvasControls.enabled = false;
+    }
+
+    public void ShowIntro()
+    {
+        _canvasIntro.enabled = true;
     }
 }
